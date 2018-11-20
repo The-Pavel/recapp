6674ab68b90f338d63b8e1d0f00dbc0a580063f2
Martin Tannenberger <martin.tannenberger@gmail.com>
2018-11-20 19:20:21 +0800

    Created a basic dashboard. Employer can add information to the dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :employers
+  devise_for :employers, controllers: { registrations: 'employers/registrations' }
   devise_for :users
   root to: 'pages#home'
+  get 'employer/dashboard/:employer_id' , to: "pages#employer_dashboard", as: 'employer_dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
