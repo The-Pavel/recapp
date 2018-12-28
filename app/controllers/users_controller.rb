722996b061b4009aff5e159380073d2d41ce1370
Martin Tannenberger <martin.tannenberger@gmail.com>
2018-12-28 19:26:05 +0800

    User can upload a video

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,27 +9,35 @@ skip_before_action :verify_authenticity_token
   end
 
   def update_video
+    sleep 1
 
     @user = User.find(params[:id])
     folder = @user.id.to_s
     results = Cloudinary::Api.resources(type: 'upload', resource_type: 'video', prefix: folder)
     resources = results["resources"]
     ids = resources.map {|res| res["url"]}
-    ids.to_a.each do |video|
-      if @user.video_array.include?(video)
-        @user.video_array
-      else
-        @user.video_array << video
-      end
+    # ids.to_a.each do |video|
+    #   if @user.video_array.include?(video)
+    #     @user.video_array
+    #   else
+    #     @user.video_array << video
+    #   end
+    # end
 
-    end
+    @user.video_array.replace(ids)
+
+
     @user.update(video_params)
     @user.save
     respond_to do |format|
       format.json {render json: @user}
       format.html {redirect_to user_dashboard_path(@user)}
     end
+
+    rescue Cloudinary::CarrierWave::UploadError
+      render plain: "File couldn't be uploaded"
   end
+
 
   private
   def user_params
