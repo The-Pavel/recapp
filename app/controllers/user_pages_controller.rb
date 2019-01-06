29459188e19bbd56736cf0e606c09e9e4e154ce8
Pavel Liser <liser.pavel@gmail.com>
2019-01-06 19:28:11 +0800

    messing around with news crawler for jobseekers

--- app/controllers/user_pages_controller.rb
+++ app/controllers/user_pages_controller.rb
@@ -6,10 +6,10 @@ class UserPagesController < ApplicationController
   def user_dashboard
     if current_user
       @user = User.find(current_user.id)
-      page = MetaInspector.new('https://www.gooverseas.com/articles')
-      page2 = MetaInspector.new('https://www.goabroad.com/articles')
-      p page.response.status
-      # p page.response.headers
+      page = MetaInspector.new('https://www.helloteacher.asia/blog')
+      # page2 = MetaInspector.new('https://www.goabroad.com/articles')
+      # p page.response.status
+      # # p page.response.headers
       go_links = page.links.http
       go_links.each do |link|
         pp = MetaInspector.new(link)
