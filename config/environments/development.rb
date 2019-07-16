f0d5a61126be48c1f3a52b85fc7c3febb44ab3f2
Pavel Liser <liser.pavel@gmail.com>
2019-07-16 14:40:55 +0800

    made compile setting to true in webpacker yml

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,10 +44,10 @@ config.webpacker.check_yarn_integrity = false
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = false
+  config.assets.debug = true
 
   # Suppress logger output for asset requests.
-  config.assets.quiet = true
+  config.assets.quiet = false
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
